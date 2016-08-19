61cc95cf86301fa9a940a54a6752197e5ea32d75
Bede Constantinides <bedeabc@gmail.com>
2016-08-19 17:25:53 +0100

    Removed unused coverage() and weights_prop()

--- offcon/offcon.py
+++ offcon/offcon.py
@@ -11,54 +11,51 @@ from collections import defaultdict
 
 
 def parse_records(sam_path, ref_max_len=100000):
-    
     with open(sam_path, 'r') as sam_fh:
         records = simplesam.Reader(sam_fh)
-
-    weights = [{'A':0,'T':0,'G':0,'C':0,'N':0} for p in range(ref_max_len)]
-    insertions = [defaultdict(int) for p in range(ref_max_len)]
-    deletions = [0] * ref_max_len
-    for i, record in enumerate(records):
-        if i % 100000 == 0 and i:
-            print('Processed', str(i), 'records...', file=sys.stderr) 
-        q_pos = 0 
-        r_pos = record.pos
-        first_iteration = True
-        for cigarette in record.cigars:
-            length, operation = cigarette
-            if operation == 'M':
-                for pos in range(length):
-                    q_nt = record.seq[q_pos]
-                    weights[r_pos][q_nt] += 1
-                    r_pos += 1
-                    q_pos += 1
-            elif operation == 'I':
-                nts = record.seq[q_pos:q_pos+length]
-                insertions[r_pos-1][nts] += 1
-                q_pos += length
-            elif operation == 'D':
-                deletions[r_pos] += 1
-                r_pos += length
-            elif operation == 'S':
-                if first_iteration:
+        weights = [{'A':0,'T':0,'G':0,'C':0,'N':0} for p in range(ref_max_len)]
+        insertions = [defaultdict(int) for p in range(ref_max_len)]
+        deletions = [0] * ref_max_len
+        for i, record in enumerate(records):
+            if i % 100000 == 0 and i:
+                print('Processed', str(i), 'records...', file=sys.stderr) 
+            q_pos = 0 
+            r_pos = record.pos
+            first_iteration = True
+            for cigarette in record.cigars:
+                length, operation = cigarette
+                if operation == 'M':
+                    for pos in range(length):
+                        q_nt = record.seq[q_pos]
+                        weights[r_pos][q_nt] += 1
+                        r_pos += 1
+                        q_pos += 1
+                elif operation == 'I':
+                    nts = record.seq[q_pos:q_pos+length]
+                    insertions[r_pos-1][nts] += 1
+                    q_pos += length
+                elif operation == 'D':
+                    deletions[r_pos] += 1
                     r_pos += length
-                    q_pos += length
-                else:
-                    q_pos += length
-            first_iteration = False
+                elif operation == 'S':
+                    if first_iteration:
+                        r_pos += length
+                        q_pos += length
+                    else:
+                        q_pos += length
+                first_iteration = False
+
+        for pos, weight in enumerate(weights[1:]):
+            if not max(weight.values()):
+                break
+        weights, insertions, deletions = weights[:pos], insertions[:pos], deletions[:pos]
+
     return weights, insertions, deletions
 
 
 def coverage(weights):
     coverage = [sum(w.values()) for w in weights]
     return coverage
-
-
-def weights_prop(weights):
-    cov = [sum(w.values()) for w in weights]
-    weights_prop = [{nt:round(wt/cov[p], 3) if cov[p] else 0 for nt, wt in wts.items() if cov[p]}
-                    for p, wts in enumerate(weights)]
-    return weights_prop
 
 
 def consensus_sequence(weights, insertions, deletions, min_coverage=1):
@@ -86,10 +83,11 @@ def consensus_sequence(weights, insertions, deletions, min_coverage=1):
 
 if __name__ == '__main__':
 
-    weights, insertions, deletions = parse_records(records)
+    sam_path = sys.argv[1]
+    weights, insertions, deletions = parse_records(sam_path)
     consensus = consensus_sequence(weights, insertions, deletions)
     
-    consensus_record = SeqRecord(Seq(cns), id='cns', description='')
-    SeqIO.write(cns_record, sys.stdout, format='fasta')
+    consensus_record = SeqRecord(Seq(consensus), id='cns', description='')
+    SeqIO.write(consensus_record, sys.stdout, format='fasta')
 
-    # print(coverage(weights))
+    print(weights)
