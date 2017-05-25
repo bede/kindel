74acbcdc69889c44387bc3d11d60707aef8a40f7
Bede Constantinides <bedeabc@gmail.com>
2017-05-25 14:10:13 +0100

    Travis

--- setup.py
+++ setup.py
@@ -3,11 +3,25 @@ import sys
 
 from setuptools import setup
 
+
 __version__ = re.search(r'__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
                         open('kindel/__init__.py').read()).group(1)
 
+
 if sys.version_info[0] < 3:
-      sys.exit('Kindel requires Python 3.0 or greater')
+      sys.exit('Kindel requires Python >= 3.3')
+
+
+CLASSIFIERS = ['Environment :: Console',
+               'Environment :: MacOS X',
+               'Intended Audience :: Science/Research',
+               'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+               'Natural Language :: English',
+               'Operating System :: POSIX :: Linux',
+               'Operating System :: MacOS :: MacOS X',
+               'Programming Language :: Python :: 3.6',
+               'Topic :: Scientific/Engineering :: Bio-Informatics']
+
 
 setup(name='kindel',
       version=__version__,
@@ -18,12 +32,12 @@ setup(name='kindel',
       license='LICENSE',
       packages=['kindel'],
       zip_safe=True,
-      install_requires=['argh==0.26.2',
-                        'tqdm==4.11.2',
-                        'numpy==1.12.0',
-                        'scipy==0.19.0',
-                        'pandas==0.19.2',
-                        'plotly==2.0.5',
-                        'biopython==1.68',
-                        'simplesam==0.0.4'],
+      install_requires=['argh>=0.26.2',
+                        'tqdm>=4.11.2',
+                        'numpy>=1.12.0',
+                        'scipy>=0.19.0',
+                        'pandas>=0.19.2',
+                        'biopython>=1.68',
+                        'simplesam>=0.0.4'],
+      extras_require={'plots': ['plotly>=2.0.0']},
       entry_points = {'console_scripts':['kindel=kindel.cli:main']})
